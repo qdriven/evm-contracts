4602f98d470b7799228d3b3a2850a8a18e84360b
DamnPat <patrickwuke@163.com>
2024-12-09 15:35:14 +0800

    cleanup projects to add new modules

--- sdk/abi/index.ts
+++
@@ -1,5 +0,0 @@
-import ERC20ABI from  "./erc20.json";
-import SWAPCONTRACTABI  from './InterestSwapStorage.json';
-import MOCKSTETHABI from "./mockStETH.json";
-
-export {ERC20ABI,SWAPCONTRACTABI,MOCKSTETHABI}
--- tasks/deploy/index.ts
+++
@@ -1,1 +0,0 @@
-import "./CallerContractDeploy.task"
